fc3fc2adf3cfbf39d27429af6f5026878f468c0a
AlecGiven <78773178+AlecGiven@users.noreply.github.com>
2021-02-12 23:24:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var name = prompt("What is my name");
+var color = prompt("What is your favorite color");
+document.body.querySelector(".message").innerHTML="my name is "+name+". My favorite color is " +color;
